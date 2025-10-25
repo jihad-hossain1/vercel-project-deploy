27b189e1304251cf4de1d1b3d6aad5df4796b249
jihad-hossain1 <jihadkhan934@gmail.com>
2025-10-25 12:13:46 +0600

    form server user

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 // Use "type: commonjs" in package.json to use CommonJS modules
 const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const prisma = require("./lib/prisma");
 const app = express();
 const port = 3000;
 
 // Define your routes
 app.get("/", (req, res) => {
   res.json({ message: "Hello from Express on Vercel!" });
+});
+
+app.get("/test", async (req, res) => {
+  const timeServer = await prisma.$queryRaw`SELECT NOW() AS now`;
+  res.json(timeServer);
 });
 
 app.listen(port, () => {
